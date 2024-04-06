291e9ff836c21698b30585994fa18dbc69ed2011
Ved <vedjain773@gmail.com>
2024-04-06 22:34:23 +0530

    changes+newchallenge

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -6,15 +6,21 @@
 const accordion = (event) => {
     const qanda = event.currentTarget;
     const elements = document.getElementsByClassName("qanda");
-    const img = qanda.getElementsByTagName("img");
     if (qanda.classList.contains("active")) {
-        img[0].src = "assets/images/icon-plus.svg"
         qanda.classList.remove("active");
     } else {
-        img[0].src = "assets/images/icon-minus.svg"
         for (i = 0 ; i <= 3; i++) {
             elements[i].classList.remove("active");
         }
         qanda.classList.add("active");
+    }
+
+    for(i = 0; i <= 3; i++) {
+        if (elements[i].classList.contains("active")) {
+            console.log("triggered");
+            elements[i].getElementsByTagName("img")[0].src = "assets/images/icon-minus.svg";
+        } else {
+            elements[i].getElementsByTagName("img")[0].src = "assets/images/icon-plus.svg";
+        }
     }
 }
