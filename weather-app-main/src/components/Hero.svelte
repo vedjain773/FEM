9b9454ef14efc19148ae64acbd2578f05038a9b8
Ved Jain <vedjain773@gmail.com>
2025-12-25 16:17:37 +0530

    added hero section for weather-app

---
+++ weather-app-main/src/components/Hero.svelte
@@ -0,0 +1,59 @@
+<div class="hero">
+    <h1>How's the sky looking today?</h1>
+    <section>
+        <div class="cont">
+            <img src="/images/icon-search.svg" alt="search" />
+            <input type="text" placeholder="Search for a place" />
+        </div>
+        <button>Search</button>
+    </section>
+</div>
+
+<style>
+    .hero {
+        width: fit-content;
+        margin: 3.25rem auto;
+    }
+
+    h1 {
+        color: var(--neutral-0);
+        font-family: var(--bg);
+        font-size: 3.25rem;
+    }
+
+    section {
+        display: flex;
+        justify-content: space-between;
+        width: 90%;
+        margin: 3.75rem auto;
+    }
+
+    .cont {
+        display: flex;
+        align-items: center;
+        background-color: var(--neutral-700);
+        padding: 0.5em 1em;
+        gap: 1em;
+        border-radius: 10px;
+    }
+
+    input,
+    button {
+        border: none;
+        font-family: var(--dm);
+        color: var(--neutral-0);
+        font-size: 1.5rem;
+    }
+
+    input {
+        outline: none;
+        background-color: transparent;
+    }
+
+    button {
+        background-color: var(--blue-500);
+        font-weight: 600;
+        padding: 0.5em 1em;
+        border-radius: 10px;
+    }
+</style>
--- weather-app-main/src/routes/+page.svelte
+++ weather-app-main/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import Nav from "../components/Nav.svelte";
+    import Hero from "../components/Hero.svelte";
 </script>
 
 <Nav />
+<Hero />
